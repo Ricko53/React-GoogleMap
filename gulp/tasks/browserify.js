bb0dba8465590204be5b98d5ed325c0cd9b90853
JinLang <2484882869@qq.com>
2015-06-25 20:49:29 +0800

    browserify

---
+++ gulp/tasks/browserify.js
@@ -0,0 +1,61 @@
+var browserify   = require('browserify');
+var watchify     = require('watchify');
+var bundleLogger = require('../util/bundleLogger');
+var gulp         = require('gulp');
+var handleErrors = require('../util/handleErrors');
+var source       = require('vinyl-source-stream');
+var config       = require('../config').browserify;
+
+gulp.task('browserify', function(callback) {
+
+	var bundleQueue = config.bunfleConfigs.length;
+
+	var browserifyThis = function(bundleConfig){
+
+		var bundler = browserify({
+
+			cache: {}, packageCache: {}, fullPaths: false,
+
+			entries: bundleConfig.entries,
+
+			extensions: config.extensions,
+
+			debug: config.debug
+		});
+
+		var bundle = function() {
+
+			bundleLogger.start(bundleConfig.outputName);
+
+			return bundler
+			  .bundle()
+			  .on('error', handleErrors)
+			  .pipe(source(bundleConfig.outputName))
+		      .pipe(gulp.dest(bundleConfig.dest))
+			  .on('end', reportFinished);
+		};
+
+		if(global.isWatching) {
+
+	      bundler = watchify(bundler);
+
+	      bundler.on('update', bundle);
+	    }
+
+	    var reportFinished = function() {
+
+	      bundleLogger.end(bundleConfig.outputName);
+
+	      if(bundleQueue) {
+	        bundleQueue--;
+	        if(bundleQueue === 0) {
+	          callback();
+	        }
+	      }
+	    };
+
+	    return bundle();		
+	};
+
+	config.bundleConfigs.forEach(browserifyThis);
+})
