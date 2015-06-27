41db812a92b717e7082d2d4b74a3e162b862e07f
JinLang <2484882869@qq.com>
2015-06-27 18:43:14 +0800

    Google map

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,3 +1,11 @@
+/* browserify task
+   ---------------
+   Bundle javascripty things with browserify!
+   This task is set up to generate multiple separate bundles, from
+   different sources, and to use Watchify when run from the default task.
+   See browserify.bundleConfigs in gulp/config.js
+*/
+
 var browserify   = require('browserify');
 var watchify     = require('watchify');
 var bundleLogger = require('../util/bundleLogger');
@@ -8,54 +16,62 @@ var config       = require('../config').browserify;
 
 gulp.task('browserify', function(callback) {
 
-	var bundleQueue = config.bunfleConfigs.length;
+  var bundleQueue = config.bundleConfigs.length;
 
-	var browserifyThis = function(bundleConfig){
+  var browserifyThis = function(bundleConfig) {
 
-		var bundler = browserify({
+    var bundler = browserify({
+      // Required watchify args
+      cache: {}, packageCache: {}, fullPaths: false,
+      // Specify the entry point of your app
+      entries: bundleConfig.entries,
+      // Add file extentions to make optional in your requires
+      extensions: config.extensions,
+      // Enable source maps!
+      debug: config.debug
+    });
 
-			cache: {}, packageCache: {}, fullPaths: false,
+    var bundle = function() {
+      // Log when bundling starts
+      bundleLogger.start(bundleConfig.outputName);
 
-			entries: bundleConfig.entries,
+      return bundler
+        .bundle()
+        // Report compile errors
+        .on('error', handleErrors)
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specifiy the
+        // desired output filename here.
+        .pipe(source(bundleConfig.outputName))
+        // Specify the output destination
+        .pipe(gulp.dest(bundleConfig.dest))
+        .on('end', reportFinished);
+    };
 
-			extensions: config.extensions,
+    if(global.isWatching) {
+      // Wrap with watchify and rebundle on changes
+      bundler = watchify(bundler);
+      // Rebundle on update
+      bundler.on('update', bundle);
+    }
 
-			debug: config.debug
-		});
+    var reportFinished = function() {
+      // Log when bundling completes
+      bundleLogger.end(bundleConfig.outputName);
 
-		var bundle = function() {
+      if(bundleQueue) {
+        bundleQueue--;
+        if(bundleQueue === 0) {
+          // If queue is empty, tell gulp the task is complete.
+          // https://github.com/gulpjs/gulp/blob/master/docs/API.md#accept-a-callback
+          callback();
+        }
+      }
+    };
 
-			bundleLogger.start(bundleConfig.outputName);
+    return bundle();
+  };
 
-			return bundler
-			  .bundle()
-			  .on('error', handleErrors)
-			  .pipe(source(bundleConfig.outputName))
-		      .pipe(gulp.dest(bundleConfig.dest))
-			  .on('end', reportFinished);
-		};
-
-		if(global.isWatching) {
-
-	      bundler = watchify(bundler);
-
-	      bundler.on('update', bundle);
-	    }
-
-	    var reportFinished = function() {
-
-	      bundleLogger.end(bundleConfig.outputName);
-
-	      if(bundleQueue) {
-	        bundleQueue--;
-	        if(bundleQueue === 0) {
-	          callback();
-	        }
-	      }
-	    };
-
-	    return bundle();		
-	};
-
-	config.bundleConfigs.forEach(browserifyThis);
-})
+  // Start bundling with Browserify for each bundleConfig specified
+  config.bundleConfigs.forEach(browserifyThis);
+});
